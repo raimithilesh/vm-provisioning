b530bd7b42ee94f618849f9bba109e20c6815d50
Rai <mithilesh.rai@capgemini.com>
2022-04-09 19:00:48 +0100

    minor change

--- src/main/java/com/ripple/vmprovisioning/controllers/UserController.java
+++ src/main/java/com/ripple/vmprovisioning/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("/api/profile")
 public class UserController {
 	@GetMapping("/all")
 	public String allAccess() {
