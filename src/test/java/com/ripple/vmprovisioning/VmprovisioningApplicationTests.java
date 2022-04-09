4a7895786af5040d98572e5325abf6655c216869
Rai <mithilesh.rai@capgemini.com>
2022-04-09 12:48:58 +0100

    Initial Springboot API commit

---
+++ src/test/java/com/ripple/vmprovisioning/VmprovisioningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ripple.vmprovisioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmprovisioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
